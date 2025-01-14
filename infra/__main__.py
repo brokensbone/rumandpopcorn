c18689ef4591c263689ffaab5d8ba246c5687d24
Edward Salkeld <edsalkeld@alcachofa.faith>
2025-01-14 20:56:15 +0000

    Pulumi code to get it built in Amplify

---
+++ infra/__main__.py
@@ -0,0 +1,34 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+from pulumi_aws import s3
+from pulumi_aws import amplify
+
+# Create an AWS resource (S3 Bucket)
+bucket = s3.BucketV2('my-bucket')
+
+# Export the name of the bucket
+pulumi.export('bucket_name', bucket.id)
+
+# Amplify App - build config
+with open("amplify.yml", "r") as file:
+    amplify_yml = file.read()
+
+# Amplify App
+conf = pulumi.Config()
+github_tok = conf.require_secret("github_token2")
+amplify_app = amplify.App('CrumAndPoppedHorn',
+    name='crumandpoppedhorn',
+    repository='https://github.com/brokensbone/rumandpopcorn',
+    build_spec=amplify_yml,
+    access_token=github_tok
+
+)
+
+main = amplify.Branch("main",
+    app_id=amplify_app.id,
+    branch_name="hugo",
+    stage="PRODUCTION",
+)
+
+pulumi.export('Domain', amplify_app.default_domain)
